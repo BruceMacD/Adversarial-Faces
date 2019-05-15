73cf92a4f34598ceed14af061654133eae7c14e0
Bruce <brucewmacdonald@gmail.com>
2019-05-14 22:41:40 -0700

    testing scale

--- image_generation/image_generator.py
+++ image_generation/image_generator.py
@@ -1,19 +1,33 @@
+import cv2
 from PIL import Image
+from facial_detection.face_detector import detect_faces
 
 OUTPUT_DIR = "../output/"
 OUTPUT_FILE_NAME = "image.png"
 OUTPUT = OUTPUT_DIR + OUTPUT_FILE_NAME
 HOG_FILE = "../data/hog_no_bg.png"
 
+hog_img = Image.open(HOG_FILE)
+width, height = hog_img.size
+
+
+def get_base_img():
+    img = Image.new('RGB', (width * 4, height * 4), (0, 0, 0))
+    return img
+
 
 def main():
-    # https://www.geeksforgeeks.org/working-images-python/
-    hog_img = Image.open(HOG_FILE)
-    width, height = hog_img.size
+    img = get_base_img()
 
-    img = Image.new('RGB', (width*4, height*4), (0, 0, 0))
-    img.paste(hog_img)
+    # fill image
+    for i in range (0, 4):
+        for j in range (0, 4):
+            img.paste(hog_img, (width*i, height*j))
+
     img.save(OUTPUT, "PNG")
+
+    # need to read the image after saving so that its the right format for opencv
+    print(len(detect_faces(cv2.imread(OUTPUT))))
 
 
 if __name__ == "__main__":
